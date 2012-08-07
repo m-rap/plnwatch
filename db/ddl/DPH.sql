0ba950bc556b9ee30593cf27fce19c6f2b00dfd0
babibubabi@gmail.com <babibubabi@gmail.com@6daff36b-5071-ff81-7222-dd3bce9d116d>
2012-08-07 08:38:49 +0000

    * fix after progres + DPH table

---
+++ db/ddl/DPH.sql
@@ -0,0 +1,22 @@
+/*
+SQLyog Enterprise - MySQL GUI v8.1 
+MySQL - 5.5.8 : Database - plnwatch
+*********************************************************************
+*/
+
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+/*Table structure for table `sorek` */
+
+CREATE TABLE `DPH` (
+  `IDPEL` int(11) NOT NULL,
+  `JMLBELI` int(11) DEFAULT NULL,
+  PRIMARY KEY (`IDPEL`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
