f20cd9fb7c1bb4cb64fd51757a04723ac8d2af4a
babibubabi@gmail.com <babibubabi@gmail.com@6daff36b-5071-ff81-7222-dd3bce9d116d>
2012-08-07 05:19:32 +0000

    * use date type

--- db/ddl/DIL.sql
+++ db/ddl/DIL.sql
@@ -1,31 +1,18 @@
--- phpMyAdmin SQL Dump
--- version 3.3.9
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Aug 07, 2012 at 05:32 
--- Server version: 5.5.8
--- PHP Version: 5.3.5
+/*
+SQLyog Enterprise - MySQL GUI v8.1 
+MySQL - 5.5.8 : Database - plnwatch
+*********************************************************************
+*/
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Database: `plnwatch`
---
+/*!40101 SET SQL_MODE=''*/;
 
--- --------------------------------------------------------
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
---
--- Table structure for table `dil`
---
+/*Table structure for table `dil` */
 
-CREATE TABLE IF NOT EXISTS `DIL` (
+CREATE TABLE `DIL` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `JENIS_MK` varchar(20) DEFAULT NULL,
   `IDPEL` int(11) DEFAULT NULL,
@@ -33,10 +20,12 @@ CREATE TABLE IF NOT EXISTS `DIL` (
   `TARIF` varchar(4) DEFAULT NULL,
   `DAYA` int(11) DEFAULT NULL,
   `LINGKUNGAN` varchar(10) DEFAULT NULL,
-  `TGLPASANG` datetime DEFAULT NULL,
+  `TGLPASANG` date DEFAULT NULL,
   `MEREK_KWH` varchar(24) DEFAULT NULL,
   `KDGARDU` varchar(30) DEFAULT NULL,
   `NOTIANG` varchar(35) DEFAULT NULL,
   `KODEAREA` varchar(5) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
--- db/ddl/SOREK.sql
+++ db/ddl/SOREK.sql
@@ -1,31 +1,18 @@
--- phpMyAdmin SQL Dump
--- version 3.3.9
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Aug 07, 2012 at 06:33 
--- Server version: 5.5.8
--- PHP Version: 5.3.5
+/*
+SQLyog Enterprise - MySQL GUI v8.1 
+MySQL - 5.5.8 : Database - plnwatch
+*********************************************************************
+*/
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Database: `plnwatch`
---
+/*!40101 SET SQL_MODE=''*/;
 
--- --------------------------------------------------------
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
---
--- Table structure for table `sorek`
---
+/*Table structure for table `sorek` */
 
-CREATE TABLE IF NOT EXISTS `SOREK` (
+CREATE TABLE `SOREK` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `BLTH` varchar(6) NOT NULL,
   `IDPEL` int(11) NOT NULL,
@@ -35,4 +22,6 @@ CREATE TABLE IF NOT EXISTS `SOREK` (
   `PEMKWH` int(11) NOT NULL,
   `KODEAREA` varchar(5) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
