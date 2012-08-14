4e79448d2e7f43d48dec1c752f261168f614f568
babibubabi@gmail.com <babibubabi@gmail.com@6daff36b-5071-ff81-7222-dd3bce9d116d>
2012-08-14 15:27:06 +0000

    + fix export to excel (html method) and able to switch method + check if already downloaded

--- web/application/views/menu1/sidebar.php
+++ web/application/views/menu1/sidebar.php
@@ -5,4 +5,9 @@ echo 'Area : ' . form_dropdown('area', $dropdownData['area']['data'], $dropdownD
  .'Rentang Tgl. Pasang : ' . form_dropdown('tglPasang', $dropdownData['tglPasang']['data'], $dropdownData['tglPasang']['selected']).'<br />'
  .form_submit('submit', ' lihat data ');
 echo form_close();
+
+if(isset($_GET['area']) && isset($_GET['daya']) && isset($_GET['tglPasang'])){
+    echo anchor('menu1/export/?area='.$_GET['area'].'&daya='.$_GET['daya'].'&tglPasang='.$_GET['tglPasang'], 'download')
+        .' (proses akan berjalan cukup lama untuk pertama kali download)';
+}
 ?>
