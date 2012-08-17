7e4fe07a58c74c94555b80bf74192c824ecbc8b1
babibubabi@gmail.com <babibubabi@gmail.com@6daff36b-5071-ff81-7222-dd3bce9d116d>
2012-08-17 04:15:16 +0000

    * small fix

--- trunk/web/application/config/config.php
+++ trunk/web/application/config/config.php
@@ -296,8 +296,8 @@ $config['global_xss_filtering'] = TRUE;
 | 'csrf_expire' = The number in seconds the token should expire.
 */
 $config['csrf_protection'] = TRUE;
-$config['csrf_token_name'] = 'csrf_test_name';
-$config['csrf_cookie_name'] = 'csrf_cookie_name';
+$config['csrf_token_name'] = 'csrfTokenName';
+$config['csrf_cookie_name'] = 'csrfCookieName';
 $config['csrf_expire'] = 7200;
 
 /*
