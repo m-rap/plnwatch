7a185b828d5486f12b457ab51a006d104d694673
mohammad.rian.ap@gmail.com <mohammad.rian.ap@gmail.com@6daff36b-5071-ff81-7222-dd3bce9d116d>
2012-08-29 15:34:45 +0000

    ditambah fungsi read ppob dengan prosedur truncate semua isi tabel dph lalu diinsertin semua record dari file mdb. ini blth-nya default ngikut dil kan?

--- trunk/app/PlnWatchDataImporter/PlnWatchDataImporter/MdbReader.cs
+++ trunk/app/PlnWatchDataImporter/PlnWatchDataImporter/MdbReader.cs
@@ -61,6 +61,7 @@ namespace PlnWatchDataImporter
         MySqlConnection mySqlConnection;
         string dmlDilFileName = "dml_dil.sql.tmp";
         string dmlSorekFileName = "dml_sorek.sql.tmp";
+        string dmlPpobFileName = "dml_ppob.sql.tmp";
         string logFileName = "readmdb.log";
         #endregion
 
@@ -467,6 +468,39 @@ namespace PlnWatchDataImporter
 
                 CultureInfo cultureInfo = CultureInfo.CreateSpecificCulture("en-US");
 
+                #region //cek tabel sorek
+                try
+                {
+                    ProgressText = "Mengecek keberadaan tabel " + sorekTableName + " di MySql dengan kode area " + SorekKodeArea;
+                    OnProgressTextChanged(null);
+                    mySqlConnection.Open();
+                    mycmd = mySqlConnection.CreateCommand();
+                    mycmd.CommandText = "SHOW TABLES WHERE Tables_in_" + mysqldb + " like '" + sorekTableName + "';";
+                    myreader = mycmd.ExecuteReader();
+                    if (myreader.Read())
+                    {
+                        myreader.Close();
+                        ProgressText = "Menghapus record sorek di tabel " + sorekTableName + " dengan kode area " + SorekKodeArea;
+                        OnProgressTextChanged(null);
+                        mycmd.CommandText = "DELETE FROM " + sorekTableName + " WHERE KODEAREA = '" + SorekKodeArea + "';";
+                        ProgressText = mycmd.ExecuteNonQuery() + " record telah dihapus";
+                        OnProgressTextChanged(null);
+                    }
+                    else
+                    {
+                        ProgressText = "Tabel " + sorekTableName + " belum ada.";
+                        OnProgressTextChanged(null);
+                        myreader.Close();
+                    }
+                    mySqlConnection.Close();
+                }
+                catch (Exception ex)
+                {
+                    ProgressText = ex.Message;
+                    OnProgressTextChanged(null);
+                }
+                #endregion
+
                 if (batchMode)
                 {
                     #region
@@ -539,22 +573,6 @@ namespace PlnWatchDataImporter
 
                     if (File.Exists(@mysqlpath))
                     {
-                        ProgressText = "Mengecek keberadaan tabel " + sorekTableName + "..";
-                        OnProgressTextChanged(null);
-                        mySqlConnection.Open();
-                        mycmd = mySqlConnection.CreateCommand();
-                        mycmd.CommandText = "SHOW TABLES WHERE Tables_in_" + mysqldb + " like '" + sorekTableName + "';";
-                        myreader = mycmd.ExecuteReader();
-                        if (myreader.Read())
-                        {
-                            myreader.Close();
-                            mycmd.CommandText = "DROP TABLE " + sorekTableName + ";";
-                            mycmd.ExecuteNonQuery();
-                        }
-                        else
-                            myreader.Close();
-                        mySqlConnection.Close();
-
                         string pArguments = "/c \"" + mysqlpath + "\" -h" + mysqlhost + " -u" + mysqluser + ((mysqlpass == "") ? "" : " -p" + mysqlpass) + " " + mysqldb + " < " + dmlSorekFileName;
 
                         ProgressText = "Mengeksekusi MySql. Silakan menunggu..";
@@ -600,20 +618,6 @@ namespace PlnWatchDataImporter
                         OnProgressTextChanged(null);
 
                         mycmd.Transaction = mySqlConnection.BeginTransaction();
-
-                        ProgressText = "Mengecek keberadaan tabel " + sorekTableName + "..";
-                        OnProgressTextChanged(null);
-
-                        mycmd.CommandText = "SHOW TABLES WHERE Tables_in_" + mysqldb + " like '" + sorekTableName + "';";
-                        myreader = mycmd.ExecuteReader();
-                        if (myreader.Read())
-                        {
-                            myreader.Close();
-                            mycmd.CommandText = "DROP TABLE " + sorekTableName + ";";
-                            mycmd.ExecuteNonQuery();
-                        }
-                        else
-                            myreader.Close();
 
                         ProgressText = "Membuat tabel " + sorekTableName + "..";
                         OnProgressTextChanged(null);
@@ -722,6 +726,191 @@ namespace PlnWatchDataImporter
         public MdbReadResult ReadPpob()
         {
             MdbReadResult result = MdbReadResult.FAILED;
+            MySqlCommand mycmd = null;
+            MySqlDataReader myreader = null;
+            try
+            {
+                int i = 0;
+                DateTime start = DateTime.Now, end, startExecuteMySql;
+                TimeSpan timeElapsed;
+
+                string ppobTh = PpobBlTh.Substring(2), ppobBl = PpobBlTh.Substring(0, 2);
+
+                ProgressText = "Membaca database. Silakan menunggu.. " + start.ToLongTimeString();
+                OnProgressTextChanged(null);
+
+                ProgressText = "Membuka koneksi Ms Access..";
+                OnProgressTextChanged(null);
+
+                oleDbConnection = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + PpobMdbPath);
+                oleDbConnection.Open();
+                OleDbCommand cmd = new OleDbCommand("SELECT IDPEL, COUNT(IDPEL) AS JMLBELI FROM " + PpobTableName + " GROUP BY IDPEL", oleDbConnection);
+                OleDbDataReader reader = cmd.ExecuteReader();
+
+                #region //truncate ppob
+                try
+                {
+                    ProgressText = "Truncate tabel PPOB di MySql";
+                    OnProgressTextChanged(null);
+                    mySqlConnection.Open();
+                    mycmd = mySqlConnection.CreateCommand();
+
+                    mycmd.CommandText = "TRUNCATE TABLE PPOB;";
+                    ProgressText = "Truncate tabel PPOB sukses";
+                    OnProgressTextChanged(null);
+
+                    mySqlConnection.Close();
+                }
+                catch (Exception ex)
+                {
+                    ProgressText = ex.Message;
+                    OnProgressTextChanged(null);
+                }
+                #endregion
+
+                if (batchMode)
+                {
+                    #region
+                    using (StreamWriter dilStreamWriter = new StreamWriter(@dmlPpobFileName))
+                    {
+                        ProgressText = "Batch Mode: true";
+                        OnProgressTextChanged(null);
+
+                        ProgressText = "Mengeksekusi reader dan menulis file dump..";
+                        OnProgressTextChanged(null);
+
+                        dilStreamWriter.WriteLine("INSERT INTO DPH (IDPEL, JMLBELI) VALUES ");
+
+                        while (reader.Read())
+                        {
+                            StringBuilder sb = new StringBuilder();
+                            if (i > 0)
+                                sb.Append(", ");
+                            sb.Append("('")
+                                .Append(reader["IDPEL"].ToString().Replace("'", "''").Replace("\\", "\\\\").Trim()).Append("', ")
+                                .Append(reader["JMLBELI"].ToString()).Append(")");
+                            dilStreamWriter.WriteLine(sb.ToString());
+
+                            string n = (++i).ToString();
+                            ProgressText = "Jumlah record:" + n + "";
+                            if (i > 1)
+                                OnProgressTextChanged(new MdbReaderProgressEventArgs(true, 14, n.Length));
+                            else
+                                OnProgressTextChanged(null);
+                        }
+                        dilStreamWriter.WriteLine(";");
+                    }
+
+                    if (File.Exists(@mysqlpath))
+                    {
+                        string pArguments = "/c \"" + mysqlpath + "\" -h" + mysqlhost + " -u" + mysqluser + ((mysqlpass == "") ? "" : " -p" + mysqlpass) + " " + mysqldb + " < " + dmlPpobFileName;
+
+                        ProgressText = "Mengeksekusi MySql. Silakan menunggu..";
+                        OnProgressTextChanged(null);
+
+                        startExecuteMySql = DateTime.Now;
+
+                        RunCmd(pArguments);
+
+                        result = MdbReadResult.SUCCESS;
+                        end = DateTime.Now;
+
+                        ProgressText = "\nSemua record PPOB berhasil dimasukkan ke MySql. " + end.ToLongTimeString();
+                        OnProgressTextChanged(null);
+                    }
+                    else
+                    {
+                        result = MdbReadResult.MYSQLNOTFOUND;
+                        end = startExecuteMySql = DateTime.Now;
+                        ProgressText = "MySql.exe tidak ditemukan. File dump akan disimpan...";
+                        OnProgressTextChanged(null);
+                    }
+                    #endregion
+                }
+                else
+                {
+                    #region
+                    startExecuteMySql = DateTime.Now;
+                    ProgressText = "Batch Mode: false";
+                    OnProgressTextChanged(null);
+
+                    try
+                    {
+                        ProgressText = "Membuka koneksi ke server MySql";
+                        OnProgressTextChanged(null);
+
+                        mySqlConnection.Open();
+                        mycmd = mySqlConnection.CreateCommand();
+                        mycmd.Connection = mySqlConnection;
+
+                        ProgressText = "Memulai transaksi";
+                        OnProgressTextChanged(null);
+
+                        mycmd.Transaction = mySqlConnection.BeginTransaction();
+
+                        ProgressText = "Mulai memasukkan data..";
+                        OnProgressTextChanged(null);
+
+                        while (reader.Read())
+                        {
+                            StringBuilder sb = new StringBuilder("INSERT INTO DPH (IDPEL, JMLBELI) VALUES  ('");
+                            
+                            sb
+                                .Append(reader["IDPEL"].ToString().Replace("'", "''").Replace("\\", "\\\\").Trim()).Append("', ")
+                                .Append(reader["JMLBELI"].ToString()).Append(");");
+
+                            mycmd.CommandText = sb.ToString();
+                            mycmd.ExecuteNonQuery();
+
+                            string n = (++i).ToString();
+                            ProgressText = "Jumlah record:" + n + "";
+                            if (i > 1)
+                                OnProgressTextChanged(new MdbReaderProgressEventArgs(true, 14, n.Length));
+                            else
+                                OnProgressTextChanged(null);
+                        }
+
+                        ProgressText = "Transaksi berakhir, melakukan commit.";
+                        OnProgressTextChanged(null);
+
+                        mycmd.Transaction.Commit();
+                        mySqlConnection.Close();
+                        result = MdbReadResult.SUCCESS;
+                    }
+                    catch (Exception ex)
+                    {
+                        ProgressText = ex.Message;
+                        OnProgressTextChanged(null);
+                        mycmd.Transaction.Rollback();
+                        result = MdbReadResult.FAILED;
+                    }
+                    end = DateTime.Now;
+                    #endregion
+                }
+
+                timeElapsed = end - start;
+                ProgressText = "Total waktu: " + timeElapsed.TotalMilliseconds;
+                OnProgressTextChanged(null);
+
+                using (StreamWriter dilStreamWriter = new StreamWriter(@logFileName, true))
+                {
+                    dilStreamWriter.WriteLine("/* ::PPOB convert:: " + DateTime.Now.ToLongDateString());
+                    dilStreamWriter.WriteLine("Records: " + i);
+                    dilStreamWriter.WriteLine("Start: " + start.ToLongTimeString());
+                    dilStreamWriter.WriteLine("Start Execute MySql: " + startExecuteMySql.ToLongTimeString());
+                    dilStreamWriter.WriteLine("End: " + end.ToLongTimeString());
+                    dilStreamWriter.WriteLine("Time Elapsed: " + timeElapsed.TotalMilliseconds);
+                    dilStreamWriter.WriteLine("*/\n");
+                }
+
+                oleDbConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                result = MdbReadResult.FAILED;
+                ProgressText = ex.Message;
+                OnProgressTextChanged(null);
+            }
             return result;
         }
         #endregion
