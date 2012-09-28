ef0c4b301f4bd70a5ad5586c5f91aa768b3c2add
babibubabi@gmail.com <babibubabi@gmail.com@6daff36b-5071-ff81-7222-dd3bce9d116d>
2012-09-28 00:56:51 +0000

    * fix Menu 3

--- trunk/web/application/views/menu3/sidebar.php
+++ trunk/web/application/views/menu3/sidebar.php
@@ -1,11 +1,11 @@
 <?php
 
 echo form_open('', array('method' => 'get'));
-echo 'Area :<br />' . form_dropdown('area', $dropdownData['area'], $this->input->get('area')) . '<br />'
- . ' Tren Pemakaian KWH :<br />' . form_dropdown('tren', $dropdownData['tren'], $this->input->get('tren')) . '<br />'
+echo 'Area :<br />' . form_dropdown('area', $dropdownData['kodearea']['list'], $dropdownData['kodearea']['input']) . '<br />'
+ . ' Tren Pemakaian KWH :<br />' . form_dropdown('tren', $dropdownData['tren']['list'], $dropdownData['tren']['input']) . '<br />'
  . form_submit('', 'lihat data', 'class="button"');
 echo form_close();
 
-echo anchor('menu3/export/?area=' . $this->input->get('area') . '&tren=' . $this->input->get('tren'), 'download')
+echo anchor('menu3/export/?area=' . $dropdownData['kodearea']['input'] . '&tren=' . $dropdownData['tren']['input'], 'download')
  . ' (proses membutuhkan waktu lebih untuk pertama kali)';
 ?>
