11ce7773378d5a3826312975dbbfe7187405a943
mohammad.rian.ap@gmail.com <mohammad.rian.ap@gmail.com@6daff36b-5071-ff81-7222-dd3bce9d116d>
2012-11-19 15:44:33 +0000

    ddl baru setelah revisi. note tentang field baru ada di field fix.txt

--- trunk/db/ddl/DIL.sql
+++ trunk/db/ddl/DIL.sql
@@ -1,19 +1,27 @@
-/*
-SQLyog Enterprise - MySQL GUI v8.1 
-MySQL - 5.5.8 : Database - plnwatch
-*********************************************************************
-*/
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-/*Table structure for table `dil` */
-
 DROP TABLE IF EXISTS `DIL`;
 
-CREATE TABLE IF NOT EXISTS `dil` (
+-- phpMyAdmin SQL Dump
+-- version 2.11.7
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 19, 2012 at 10:40 PM
+-- Server version: 5.0.51
+-- PHP Version: 5.2.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Database: `plnwatch`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dil`
+--
+
+CREATE TABLE IF NOT EXISTS `DIL` (
   `JENIS_MK` varchar(20) default NULL,
   `IDPEL` varchar(12) NOT NULL,
   `NAMA` varchar(25) default NULL,
@@ -33,7 +41,8 @@ CREATE TABLE IF NOT EXISTS `dil` (
   `KDGARDU` varchar(30) default NULL,
   `NOTIANG` varchar(35) default NULL,
   `KODEAREA` varchar(5) default NULL,
+  `TGLPDL` datetime default NULL,
+  `TGLNYALA_PB` date default NULL,
+  `TGLRUBAH_MK` date default NULL,
   PRIMARY KEY  (`IDPEL`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
--- trunk/db/ddl/SOREK.sql
+++ trunk/db/ddl/SOREK.sql
@@ -1,19 +1,15 @@
+DROP TABLE IF EXISTS `SOREK_062012`;
+
 -- phpMyAdmin SQL Dump
 -- version 2.11.7
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 10, 2012 at 04:19 PM
+-- Generation Time: Nov 19, 2012 at 10:42 PM
 -- Server version: 5.0.51
 -- PHP Version: 5.2.6
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `plnwatch`
@@ -22,14 +18,18 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sorek`
+-- Table structure for table `sorek_062012`
 --
 
-CREATE TABLE IF NOT EXISTS `SOREK_2012_07` (
+CREATE TABLE IF NOT EXISTS `SOREK_062012` (
   `IDPEL` varchar(12) NOT NULL,
   `TGLBACA` date default NULL,
   `PEMKWH` double default NULL,
   `KODEAREA` varchar(5) default NULL,
-  `JAMNYALA` double default NULL,
+  `JAMNYALA` int(11) default NULL,
+  `FAKM` varchar(10) default NULL,
+  `KWHLWBP` int(11) default NULL,
+  `KWHWBP` int(11) default NULL,
+  `KWHKVARH` int(11) default NULL,
   PRIMARY KEY  (`IDPEL`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
