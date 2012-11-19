8a2f526d462b5fe43020ad159a1a1f90c9aea511
mohammad.rian.ap@gmail.com <mohammad.rian.ap@gmail.com@6daff36b-5071-ff81-7222-dd3bce9d116d>
2012-11-19 17:17:06 +0000

    gabung field alamat di dil, gabung field tglbayar di dph, tambah field tren di sorek

--- db/ddl/DIL.sql
+++ db/ddl/DIL.sql
@@ -1,11 +1,10 @@
 DROP TABLE IF EXISTS `DIL`;
-
 -- phpMyAdmin SQL Dump
 -- version 2.11.7
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 19, 2012 at 10:40 PM
+-- Generation Time: Nov 20, 2012 at 12:14 AM
 -- Server version: 5.0.51
 -- PHP Version: 5.2.6
 
@@ -21,18 +20,13 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Table structure for table `dil`
 --
 
-CREATE TABLE IF NOT EXISTS `DIL` (
+CREATE TABLE IF NOT EXISTS `dil` (
   `JENIS_MK` varchar(20) default NULL,
   `IDPEL` varchar(12) NOT NULL,
   `NAMA` varchar(25) default NULL,
   `TARIF` varchar(4) default NULL,
   `DAYA` int(11) default NULL,
-  `PNJ` varchar(2) default NULL,
-  `NAMAPNJ` varchar(62) default NULL,
-  `NOBANG` varchar(3) default NULL,
-  `RT` varchar(3) default NULL,
-  `RW` varchar(2) default NULL,
-  `LINGKUNGAN` varchar(10) default NULL,
+  `ALAMAT` varchar(70) default NULL,
   `NOTELP` varchar(20) default NULL,
   `KODEPOS` int(5) default NULL,
   `TGLPASANG_KWH` date default NULL,
--- db/ddl/SOREK.sql
+++ db/ddl/SOREK.sql
@@ -1,11 +1,10 @@
 DROP TABLE IF EXISTS `SOREK_062012`;
-
 -- phpMyAdmin SQL Dump
 -- version 2.11.7
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 19, 2012 at 10:42 PM
+-- Generation Time: Nov 20, 2012 at 12:15 AM
 -- Server version: 5.0.51
 -- PHP Version: 5.2.6
 
@@ -21,7 +20,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Table structure for table `sorek_062012`
 --
 
-CREATE TABLE IF NOT EXISTS `SOREK_062012` (
+CREATE TABLE IF NOT EXISTS `sorek_062012` (
   `IDPEL` varchar(12) NOT NULL,
   `TGLBACA` date default NULL,
   `PEMKWH` double default NULL,
@@ -31,5 +30,6 @@ CREATE TABLE IF NOT EXISTS `SOREK_062012` (
   `KWHLWBP` int(11) default NULL,
   `KWHWBP` int(11) default NULL,
   `KWHKVARH` int(11) default NULL,
+  `TREN` enum('naik','turun','flat') NOT NULL,
   PRIMARY KEY  (`IDPEL`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
