6cfd5a285696736134a4a086ef0ff945303929fd
mohammad.rian.ap@gmail.com <mohammad.rian.ap@gmail.com@6daff36b-5071-ff81-7222-dd3bce9d116d>
2012-11-19 15:44:33 +0000

    ddl baru setelah revisi. note tentang field baru ada di field fix.txt

--- db/ddl/DPH.sql
+++ db/ddl/DPH.sql
@@ -1,24 +1,32 @@
-/*
-SQLyog Enterprise - MySQL GUI v8.1 
-MySQL - 5.5.8 : Database - plnwatch
-*********************************************************************
-*/
-
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-/*Table structure for table `DPH` */
-
 DROP TABLE IF EXISTS `DPH`;
 
-CREATE TABLE `DPH` (
+-- phpMyAdmin SQL Dump
+-- version 2.11.7
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 19, 2012 at 10:41 PM
+-- Server version: 5.0.51
+-- PHP Version: 5.2.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Database: `plnwatch`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dph`
+--
+
+CREATE TABLE IF NOT EXISTS `dph` (
   `IDPEL` varchar(12) NOT NULL,
   `JMLBELI` int(11) default NULL,
+  `PEMKWH` double default NULL,
+  `RPTAG` double default NULL,
+  `TGLBAYAR` date default NULL,
+  `JAMBAYAR` time default NULL,
   PRIMARY KEY  (`IDPEL`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
